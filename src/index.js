09d5d16a3b7cbefcbff02468fa881d370aef0b25
Levron Jérémy <jeremylevron@19h47.fr>
2020-04-03 15:08:08 +0200

    :arrow_up: Upgrade dependencies

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 /**
  *
  * @file index.js
- * @author Jérémy Levron <jeremylevron@19h47.fr> (http://19h47.fr)
+ * @author Jérémy Levron <jeremylevron@19h47.fr> (https://19h47.fr)
  */
 import raf from 'raf';
 import VirtualScroll from 'virtual-scroll';
